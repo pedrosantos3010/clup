c6fa50ee7e05c86e5201ed812bfb85c726283459
pedrosantos3010 <pedrosantos3010@gmail.com>
2022-09-18 16:20:29 -0300

    🎨 refactor selectOne to respect eslint

--- src/framework/selectOne.ts
+++ src/framework/selectOne.ts
@@ -1,26 +1,27 @@
 import { TerminalView } from "../view/TerminalView";
 
 type SelectableItem = {
-  index: number;
-  id: string;
-  name: string;
+    index: number;
+    id: string;
+    name: string;
 };
 
 export async function fetchAndSelectOne<T extends SelectableItem>(
-  fetch: () => Promise<T[] | null>,
-  terminal: TerminalView,
-  label: string
+    fetch: () => Promise<T[] | null>,
+    terminal: TerminalView,
+    label: string
 ): Promise<T> {
-  const item = await terminal.waitAction(fetch());
+    const item = await terminal.waitAction(fetch());
 
-  if (!item) {
-    terminal.endWithError(`no ${label} was found`);
-  }
+    if (!item) {
+        terminal.endWithError(`no ${label} was found`);
+        throw new Error("Failed to end!");
+    }
 
-  const selectedSpace = await terminal.selectItem(
-    item!.map((s) => s.name),
-    { header: label }
-  );
+    const selectedSpace = await terminal.selectItem(
+        item.map((s) => s.name),
+        { header: label }
+    );
 
-  return item![selectedSpace.index];
+    return item[selectedSpace.index];
 }
