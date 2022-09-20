fb6f352d6a9e0b921ba5c4f6ee9c5eea1d0633ca
pedrosantos3010 <pedrosantos3010@gmail.com>
2022-09-19 21:33:48 -0300

    🐛 fix types at fetchSpaces

--- src/utils/fetchSpaces.ts
+++ src/utils/fetchSpaces.ts
@@ -2,17 +2,26 @@ import { CLICKUP_BASE_URL } from "../ClickupConfig";
 import { request } from "./fetch";
 
 export const fetchSpaces = async (
-  apiKey: string
-): Promise<Array<{
-  index: number;
-  id: string;
-  name: string;
-}> | null> => {
-  const result = await request(`${CLICKUP_BASE_URL}/team`, apiKey);
+    apiKey: string
+): Promise<Array<Team> | undefined> => {
+    const result = await request<TeamsResponse>(
+        `${CLICKUP_BASE_URL}/team`,
+        apiKey
+    );
 
-  return result?.data?.teams?.map((item, index) => ({
-    index: index,
-    id: item.id,
-    name: item.name,
-  }));
+    return result?.data?.teams?.map((item, index) => ({
+        index: index,
+        id: item.id,
+        name: item.name,
+    }));
 };
+
+type Team = {
+    index: number;
+    id: string;
+    name: string;
+};
+
+interface TeamsResponse {
+    teams: Array<Team>;
+}
